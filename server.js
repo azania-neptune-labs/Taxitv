9505a767c407a8f7e0b47f9a76de12945c6d2f63
SkywalkingZulu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-09-17 13:47:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express();
 app.get('/', (req, res) => res.redirect('https://skywalkingzulu.github.io/Taxitv/primeframe.html'))
 
 const server = app.listen(9088, () => {
+  /*
   ngrok.connect(9088).then(ngrokUrl => {
     endpoint = ngrokUrl
     console.log(`Verification Service running, open at ${endpoint}`)
   })
+  */
 })
