39a3d3b6174a126a67761aaf6495bf2bb5b1d8a9
SkywalkingZulu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-09-26 12:39:35 +0200

    Add files via upload

---
+++ subpro.py
@@ -0,0 +1,17 @@
+
+import os
+
+     #   PORT = 9000
+
+os.system("node serve.js")
+
+#Handler = http.server.SimpleHTTPRequestHandler
+
+
+#with socketserver.TCPServer(("", 9000), Handler) as httpd:
+    #print("serving at port", 9000)
+    #httpd.serve_forever() 
+
+    
+
+   
