eaa1db00927cb011c53e3d0983834bfb8cea3803
SkywalkingZulu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-10-02 15:10:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+ /*
 const ngrok = require('ngrok');
 const express = require('express')
 
@@ -7,10 +8,25 @@ const app = express();
 app.get('/', (req, res) => res.redirect('https://skywalkingzulu.github.io/Taxitv/primeframe.html'))
 
 const server = app.listen(9088, () => {
-  /*
+ 
   ngrok.connect(9088).then(ngrokUrl => {
     endpoint = ngrokUrl
     console.log(`Verification Service running, open at ${endpoint}`)
   })
-  */
+ 
 })
+ */
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.redirect('https://skywalkingzulu.github.io/Taxitv/primeframe.html');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
+
