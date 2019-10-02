0d9b1f2045c98e56cdff11902906f2d34364d9b6
SkywalkingZulu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-10-02 15:08:40 +0200

    Update subpro.py

--- subpro.py
+++ subpro.py
@@ -3,9 +3,9 @@ import os
 
      #   PORT = 9000
 
-os.system("forever start server.js nohup")
-os.system(" authtoken 1RNC9uqE9DxrHqtyh90QCigtTru_6bEGQgZQLN4eSj1QZAVKq")
-os.system(" ngrok http -subdomain=skywalkerzulu 3000")
+os.system("forever start server.js nohup")#This is for no output
+os.system("sudo ngrok authtoken 1RNC9uqE9DxrHqtyh90QCigtTru_6bEGQgZQLN4eSj1QZAVKq nohup")#This is for the auth
+os.system("sudo ngrok http -subdomain=skywalkerzulu 3000")#This should be the end result
 
 #Handler = http.server.SimpleHTTPRequestHandler
 
