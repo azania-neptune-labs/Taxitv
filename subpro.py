43138016ee380927f925fca60bcbd5aa35a2b4d0
SkywalkingZulu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-10-02 15:15:44 +0200

    Update subpro.py

--- subpro.py
+++ subpro.py
@@ -3,7 +3,7 @@ import os
 
      #   PORT = 9000
 
-os.system("forever start server.js nohup")#This is for no output
+os.system("forever start /home/pi/Taxitv-master/server.js nohup")#This is for no output
 os.system("sudo ngrok authtoken 1RNC9uqE9DxrHqtyh90QCigtTru_6bEGQgZQLN4eSj1QZAVKq nohup")#This is for the auth
 os.system("sudo ngrok http -subdomain=skywalkerzulu 3000")#This should be the end result
 
